f4cb9723700d8b0707bdd5d03fe947bd737cd294
shegaw07 <162834998+shegaw07@users.noreply.github.com>
2024-05-12 18:07:52 -0400

    Header and Footer

---
+++ netflix-clone/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
